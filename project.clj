2cf6d7ac3c40cdd4ac52cc62e517c1843c4c60a5
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2019-08-03 18:02:37 +0200

    Update version and URL in project file

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lein-antlr "0.3.0"
+(defproject lein-antlr "0.4.0"
   :description "Generate source code from ANTLR grammars in Leiningen."
   :dependencies [[org.antlr/antlr4 "4.7.2"]]
   :profile {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}}
-  :url "http://github.com/alexhall/lein-antlr"
+  :url "http://github.com/thierrymarianne/lein-antlr"
   :eval-in-leiningen true
   :license {:name "Apache Software License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"
